479880b0bbcfd7832f77dd05cdef0e14edd59880
Joel Nitta <joelnitta@gmail.com>
2021-12-03 10:26:47 +0900

    Add 'type' column to dct_terms

--- data-raw/dct_terms.R
+++ data-raw/dct_terms.R
@@ -9,8 +9,12 @@ dct_terms <-
 	# Strip out the term from the URL
 	dplyr::mutate(
 		group = stringr::str_to_lower(group),
-		term = stringr::str_split(iri, "\\/") |> purrr::map_chr(dplyr::last)) |>
-	dplyr::select(group, term)
+		term = stringr::str_split(iri, "\\/") |> purrr::map_chr(dplyr::last),
+		# Treat 'date' as 'character' to avoid hassle of formatting dates
+		# same for 'uri'
+		type = tidyr::replace_na(type, "character") |> stringr::str_replace_all("date|uri", "character")
+		) |>
+	dplyr::select(group, term, type)
 
 # Add two attributes: date-time of retrieval, URL where terms originate
 attributes(dct_terms) <- c(
