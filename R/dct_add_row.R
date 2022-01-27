78010b1e7b2ab2fcf16db64d16475d70a690b50e
Joel Nitta <joelnitta@gmail.com>
2022-01-27 12:36:38 +0900

    Make dct_add_row more flexible

--- R/dct_add_row.R
+++ R/dct_add_row.R
@@ -4,62 +4,82 @@
 #' @param taxon_id taxonID to use for new entry; optional, will be
 #' assigned automatically if not provided.
 #' @param sci_name scientificName to use for new entry
-#' @param tax_status taxonomicStatus to use for new entry
-#' @param usage_id acceptedNameUsageID to use for new entry; optional
 #' @param strict Logical; should taxonomic checks be run on the updated
 #' taxonomic database?
+#' @param ... Additional data to include, specified as sets of named
+#' character strings; e.g., `parentNameUsageID = "6SH4"`. The name of
+#' each string must be a valid column name for data in Darwin Core format.
 #'
 #' @return Dataframe; taxonomic database in Darwin Core format
+#' @examples
+#' tibble::tibble(
+#'   taxonID = "123",
+#'   scientificName = "Foogenus barspecies"
+#' ) |>
+#' dct_add_row(
+#'   sci_name = "Foogenus barspecies var. bla",
+#'   parentNameUsageID = "123",
+#'   nameAccordingTo = "me"
+#' )
 #' @autoglobal
 #' @export
 dct_add_row <- function(
-	tax_dat, taxon_id = NULL,
-	sci_name, tax_status,
-	usage_id = NULL,
-	strict = FALSE
-) {
+	tax_dat, taxon_id = NULL, sci_name, strict = TRUE, ...) {
 
 	# Check input
 	assertthat::assert_that(assertthat::is.string(sci_name))
-	assertthat::assert_that(assertthat::is.string(tax_status))
+	if (!is.null(taxon_id)) assertthat::assert_that(assertthat::is.string(taxon_id)) #nolint
+	assertthat::assert_that(assertthat::is.flag(strict))
+
 	# Assign default taxon_id as hash of sci name
 	if (is.null(taxon_id)) taxon_id <- digest::digest(sci_name)
-	if (is.null(usage_id)) usage_id <- NA_character_
+
 	# Create new row to add
 	new_row <- tibble::tibble(
 		taxonID = taxon_id,
-		acceptedNameUsageID = usage_id,
-		taxonomicStatus = tax_status,
 		scientificName = sci_name,
 		modified = as.character(Sys.time())
 	)
+
+	# Add other data
+	other_data <- tibble::tibble(...)
+
+	if (nrow(other_data) > 0) {
+		# Run checks on other data
+		dct_validate(
+			other_data,
+			check_taxon_id = FALSE,
+			check_mapping = FALSE,
+			check_taxonomic_status = FALSE,
+			check_acc_syn_diff = FALSE,
+			check_col_names = TRUE)
+
+		assertthat::assert_that(
+			nrow(other_data) == 1,
+			msg = "Only one row of additional data may be added")
+
+		assertthat::assert_that(
+			!any(colnames(other_data) %in% c(
+				"taxonID", "scientificName"
+			)),
+			msg = "Names of additional data may not include 'taxonID' or 'scientificName'"
+		)
+
+		new_row <- dplyr::bind_cols(new_row, other_data)
+
+	}
+
 	# Check for duplicated ID
 	overlap_id <- dplyr::inner_join(tax_dat, new_row, by = "taxonID")
 	assertthat::assert_that(nrow(overlap_id) == 0,
 													msg = "`tax_dat` already contains that `taxonID`"
 	)
-	# Check for duplicated data
-	overlap_rows <- dplyr::inner_join(tax_dat, new_row, by = c(
-		"taxonID", "acceptedNameUsageID", "taxonomicStatus", "scientificName"
-	))
-	assertthat::assert_that(nrow(overlap_rows) == 0,
-													msg = "`tax_dat` already contains data to add"
-	)
-	# Check that usage ID exists, if applicable
-	if (!is.na(usage_id)) {
-		overlap_usage_id <- dplyr::inner_join(
-			tax_dat, new_row, by = c(taxonID = "acceptedNameUsageID"))
-		assertthat::assert_that(
-			nrow(overlap_usage_id) == 1,
-			msg = "`usage_id` does not match exactly one existing `taxonID`"
-		)
-	}
+
 	# Add new row
-	res <- tax_dat |>
-		tibble::add_row(new_row)
+	res <- dplyr::bind_rows(tax_dat, new_row)
 
 	# Optionally run taxonomic database checks
-	if (isTRUE(strict)) res <- dct_validate(res)
+	if (isTRUE(strict)) dct_validate(res)
 
 	res
 }
