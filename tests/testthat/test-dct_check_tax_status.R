7dbac20967211dd55caae379d27f0493add45e05
Joel Nitta <joelnitta@gmail.com>
2022-11-18 15:12:23 +0900

    Remove redundant tests

--- tests/testthat/test-dct_check_tax_status.R
+++ tests/testthat/test-dct_check_tax_status.R
@@ -1,15 +1,8 @@
-# Clear default value for VALID_TAX_STATUS
-Sys.unsetenv("VALID_TAX_STATUS")
-
 test_that("correctly formatted data does not error", {
   good_dat <- tibble::tribble(
     ~taxonID, ~acceptedNameUsageID, ~taxonomicStatus, ~scientificName,
     "1", NA, "accepted", "Species foo",
     "2", "1", "synonym", "Species bar"
-  )
-  expect_equal(
-    check_tax_status_valid(good_dat),
-    good_dat
   )
   expect_equal(
     dct_check_tax_status(good_dat),
@@ -23,30 +16,6 @@ test_that("Check for 'taxonomicStatus in valid values' works", {
     "1", NA, "accepted", "Species foo",
     "2", "1", "synonym", "Species bar",
     "3", NA, "foo", "Species bat"
-  )
-  expect_error(
-    check_tax_status_valid(bad_dat),
-    paste0(
-      "check_tax_status failed.*",
-      "taxonID detected whose taxonomicStatus is not in valid_tax_status.*",
-      "Bad taxonID\\: 3.*",
-      "Bad taxonomicStatus\\: foo"
-    )
-  )
-  expect_equal(
-    suppressWarnings(
-      check_tax_status_valid(bad_dat, on_fail = "summary")
-    ),
-    tibble::tibble(
-      taxonID = "3",
-      scientificName = "Species bat",
-      taxonomicStatus = "foo",
-      error = paste(
-        "taxonID detected whose taxonomicStatus is not in",
-        "valid_tax_status (accepted, synonym, variant, NA)"
-      ),
-      check = "check_tax_status"
-    )
   )
   expect_error(
     dct_check_tax_status(bad_dat),
@@ -73,6 +42,3 @@ test_that("Check for 'taxonomicStatus in valid values' works", {
     )
   )
 })
-
-# Clear default value for VALID_TAX_STATUS
-Sys.unsetenv("VALID_TAX_STATUS")
