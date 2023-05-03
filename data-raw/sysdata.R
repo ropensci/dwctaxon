b67d0c4cc4ac2f68abf085acfa4e2193de3c8c35
Joel Nitta <joelnitta@gmail.com>
2023-05-03 15:10:59 +0900

    Allow for self-mapping between taxonID and acceptedNameUsageID for accepted names. Fixes #58

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,9 +1,9 @@
 # Set up data for testing check_mapping_to_self()
 bad_dat_dup_taxid <- tibble::tribble(
-  ~taxonID, ~acceptedNameUsageID, ~scientificName,
-  "1", NA, "Species foo",
-  "2", "1", "Species bar",
-  "3", "3", "Species bat"
+  ~taxonID, ~acceptedNameUsageID, ~scientificName, ~taxonomicStatus,
+  "1", NA, "Species foo", "Accepted",
+  "2", "1", "Species bar", "Synonym",
+  "3", "3", "Species bat", "Synonym"
 )
 bad_dat_dup_taxid_cols <- c(
   "acceptedNameUsageID", "parentNameUsageID", "originalNameUsageID"
@@ -35,5 +35,6 @@ usethis::use_data(
   bad_dat_dup_taxid_cols,
   bad_dat_dup_taxid,
   bad_dat_missing_taxid,
-  internal = TRUE
+  internal = TRUE,
+  overwrite = TRUE
 )
